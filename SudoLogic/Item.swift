4592e568a15a3162401eb927ca0203223d1e239d
max <maxwell20365@gmail.com>
2026-08-04 22:27:47 +0800

    Initial Commit

---
+++ SudoLogic/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SudoLogic
+//
+//  Created by Maxwell on 2026/8/4.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
